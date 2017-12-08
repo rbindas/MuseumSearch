c6e633bb04edf84d997658c369025869c7e8265a
mizhanh <31321795+mizhanh@users.noreply.github.com>
2017-12-08 09:48:28 -0500

    modified html, css and .js files to clean up codes and ensure everything works

--- assets/javascript/museum.js
+++ assets/javascript/museum.js
@@ -94,6 +94,7 @@ $("#add-city").on("click", function(event){
         $("#museum-list").append($museumDiv);
 
         createMarker(results[i]);
+
       }
     }
   }
@@ -102,8 +103,79 @@ $("#add-city").on("click", function(event){
    
 
 //==============================================================
-// TABLE
+// FIREBASE TABLE
 //==============================================================
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBIh3Eaa12mCe_gUGOPUMVE9JT_-1oT_eo",
+    authDomain: "museumsearch-4d634.firebaseapp.com",
+    databaseURL: "https://museumsearch-4d634.firebaseio.com",
+    projectId: "museumsearch-4d634",
+    storageBucket: "museumsearch-4d634.appspot.com",
+    messagingSenderId: "440322454720"
+  };
+  
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  //Button to add museums
+  $("#add-museum").on("click", function(event) {
+  event.preventDefault();
+
+    // Grabs user input
+    var museumName = $("#museum-name-input").val().trim();
+    var museumCity = $("#city-input").val().trim();
+    var museumAddress = $("#address-input").val().trim();
+    var visitDate = $("#time-input").val().trim();
+    var notes = $("#notes-input").val().trim();
+  
+  
+    // Uploads data to the database
+    database.ref().push({
+      name: museumName,
+      destin: museumCity,
+      address: museumAddress,
+      date: visitDate, 
+      note: notes   
+    });
+
+    // Logs everything to console
+    console.log(museumName);
+    console.log(museumCity);
+    console.log(museumAddress);
+    console.log(visitDate);
+    console.log(notes);
+  
+    //Empty out form
+    $("#museum-name-input").val("");
+    $("#city-input").val("");
+    $("#address-input").val("");
+    $("#time-input").val("");
+    $("#notes-input").val("");
+ 
+  });
+
+  //  Firebase event for adding train to the database & row in the html when a user adds an entry
+  database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+    console.log(childSnapshot.val());
+
+    // Store everything into a variable.
+    var museumName = childSnapshot.val().name;
+    var museumCity = childSnapshot.val().destin;
+    var museumAddress = childSnapshot.val().address;
+    var visitDate = childSnapshot.val().date;
+    var notes = childSnapshot.val().note;
+ 
+        
+    $("#visit-schedule > tbody").append("<tr><td>" + museumName + "</td><td>" + museumCity + "</td><td>" + museumAddress + "</td><td>" + visitDate +  "</td><td>" + notes + "</td></tr>");  
+    
+    // Handle the errors
+    }, function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+  });
 
 
 
